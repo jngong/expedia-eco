48a5f89b21fa239112983298d6c8bfbec8b3d2bb
jngong <34215359+jngong@users.noreply.github.com>
2020-02-16 15:27:29 -0500

    drafted controllers for remaining CRUD functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,4 @@
-// TODO: Will need to confirm with Disney what the model names are.
 const { Cities, Hotels, Rooms } = require('../models')
-
 
 // GET request for cities. Refer to "City" model. Should I join the Hotels model? Route: GET /api/cities
 const getAllCities = async (req, res) => {
@@ -12,7 +10,7 @@ const getAllCities = async (req, res) => {
     }
 }
 
-// GET request for hotels by city id. Will refer to "Hotel" model, "city_id" foreign key to match. Need to check models to ensure consistency.
+// GET request for hotels by city id. Will refer to "Hotels" model, "city_id" foreign key to match. Need to check models to ensure consistency.
 // Route: GET /api/cities/:city_id/hotels
 
 const getAllHotelsByCity = async (req, res) => {
@@ -32,7 +30,7 @@ const getAllHotelsByCity = async (req, res) => {
     }
 }
 
-// GET request for a single hotel by hotel id. Will refer to the Hotel model
+// GET request for a single hotel by hotel id. Will refer to the Hotels model
 // Route: GET /api/hotels/:hotel_id
 
 const getHotelDetails = async (req, res) => {
@@ -41,7 +39,10 @@ const getHotelDetails = async (req, res) => {
         const hotel = await Hotels.findOne({
             where: {
                 id: hotel_id
-            }
+            }, 
+            include: [
+                { model: Rooms }
+            ]
         })
         if (hotel) {
             return res.status(200).json({hotel: hotel})
@@ -52,7 +53,7 @@ const getHotelDetails = async (req, res) => {
     }
 }
 
-// GET request for all rooms within a hotel. Refer to the "Room" model and match on "hotel_id" foreign key. Need to check that the names all match.
+// GET request for all rooms within a hotel. Refer to the "Rooms" model and match on "hotel_id" foreign key.
 // Route: GET /api/hotels/:hotel_id/rooms
 
 const getAllRoomsByHotel = async (req, res) => {
@@ -83,7 +84,7 @@ const getRoomDetails = async (req, res) => {
         if (room) {
             return res.status(200).json({room: room})
         }
-        return res.status(404).send('That room does not exist.')
+        return res.status(404).send(`The room with id ${room_id} does not exist.`)
     } catch (error) {
         return res.json(500).send(error.message)
     }
@@ -94,29 +95,48 @@ const getRoomDetails = async (req, res) => {
 
 const createHotel = async (req, res) => {
     try {
-        
+      const hotel = await Hotels.create(req.body);
+      return res.status(201).json({ hotel })  
     } catch (error) {
-        
+        console.log(error)
+        return res.status(500).send(error.message)
     }
 }
 
 // PUT request for updating an existing hotel
+// Route: PUT /api/hotels/:hotel_id
 
 const updateHotel = async (req, res) => {
     try {
-        
+      const { hotel_id } = req.params
+      const updated = await Hotels.update(req.body, {
+          where: { id: hotel_id }
+      })
+      if (updated) {
+          const updatedHotel = await Hotels.findOne({where: {id: hotel_id}})
+          return res.status(200).json({ hotel: updatedHotel })
+      }
+      throw new Error(`Hotel with id ${hotel_id} was not found.`)
     } catch (error) {
-        
+        return res.status(500).send(error.message)   
     }
 }
 
 // DELETE request for deleting a hotel
+// Route: DELETE /api/hotels/:hotel_id
 
 const deleteHotel = async (req, res) => {
     try {
-        
+        const { hotel_id } = req.params
+        const deleted = await Hotels.destroy({
+            where: { id: hotel_id}
+        })
+        if (deleted) {
+            return res.status(204).send(`Hotel with id ${hotel_id} was deleted.`)
+        }
+        throw new Error(`Hotel with id ${hotel_id} was not found.`)
     } catch (error) {
-        
+        return res.status(500).send(error.message)
     }
 }
 
@@ -124,7 +144,6 @@ const deleteHotel = async (req, res) => {
 Post-MVP:
 createRoomInHotel
 updateRoomInHotel
-
  */
 
 module.exports = {
