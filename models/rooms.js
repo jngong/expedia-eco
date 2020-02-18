10dd9e3eb60535a4bfc48676197245a33e1d10fb
Jeffrey Lan <jtlan.sf@gmail.com>
2020-02-18 10:22:33 -0500

    corrected typo in models/rooms.js

--- models/rooms.js
+++ models/rooms.js
@@ -1,20 +1,24 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Rooms = sequelize.define('Rooms', {
-    hotel_id: DataTypes.INTEGER,
-    type: DataTypes.STRING,
-    list_price: DataTypes.CHAR,
-    current_price: DataTypes.CHAR,
-    availability: DataTypes.BOOLEAN,
-    beds: DataTypes.STRING,
-    sleeps: DataTypes.INTEGER
-  }, {});
+  const Rooms = sequelize.define(
+    "Rooms",
+    {
+      hotel_id: DataTypes.INTEGER,
+      type: DataTypes.STRING,
+      list_price: DataTypes.CHAR,
+      current_price: DataTypes.CHAR,
+      availability: DataTypes.BOOLEAN,
+      beds: DataTypes.STRING,
+      sleeps: DataTypes.INTEGER
+    },
+    {}
+  );
   Rooms.associate = function(models) {
     // associations can be defined here
     Rooms.belongsTo(models.Hotels, {
-      foreignKey: 'Hotel_id',
-      onDelete: 'CASCADE'
-    })
+      foreignKey: "hotel_id",
+      onDelete: "CASCADE"
+    });
   };
   return Rooms;
 };
