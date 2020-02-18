b7eedf54a37b47d3a63b4f508a0fcd5210eb3557
Ro <hohenkirkrhol@gmail.com>
2020-02-18 10:06:15 -0500

    Completed Seed Files.

--- seeders/20200217144709-cities.js
+++ seeders/20200217144709-cities.js
@@ -41,12 +41,7 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      return queryInterface.bulkDelete('Cities', null, {});
+    
   }
 };
--- seeders/20200217145332-rooms.js
+++ seeders/20200217145332-rooms.js
@@ -4,63 +4,73 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
     return queryInterface.bulkInsert('Rooms', [{
-      "hotel_id": "1",
-      "type": "",
-      "list_price": "",
-      "current_price": "",
-      "availability": "",
-      "beds": "",
-      "sleeps": "",
+      "hotel_id": 1,
+      "type": "King-Suite",
+      "list_price": "$300.00/nightly",
+      "current_price": "$200.00/nightly",
+      "availability": true,
+      "beds": "1 King-Sized Beds",
+      "sleeps": 3,
       createdAt: new Date(),
       updatedAt: new Date()
     },
       
       {
-        "hotel_id": "2",
-        "type": "",
-        "list_price": "",
-        "current_price": "",
-        "availability": "",
-        "beds": "",
-        "sleeps": "",
+        "hotel_id": 2,
+        "type": "Queen-Suite",
+        "list_price": "$400.00/nightly",
+        "current_price": "350.00/nightly",
+        "availability": false,
+        "beds": "1 Queen-Sized Bed",
+        "sleeps": 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
 
       {
-        "hotel_id": "3",
-        "type": "",
-        "list_price": "",
-        "current_price": "",
-        "availability": "",
-        "beds": "",
-        "sleeps": "",
+        "hotel_id": 3,
+        "type": "King-Suite",
+        "list_price": "300.00/nightly",
+        "current_price": "299.99/nightly",
+        "availability": true,
+        "beds": "2 King-Sized Beds",
+        "sleeps": 5,
         createdAt: new Date(),
         updatedAt: new Date()
       },
 
-
       {
-        "hotel_id": "4",
-        "type": "",
-        "list_price": "",
-        "current_price": "",
-        "availability": "",
-        "beds": "",
-        "sleeps": "",
+        "hotel_id": 3,
+        "type": "King-Suite",
+        "list_price": "$300.00/nightly",
+        "current_price": "$200.00/nightly",
+        "availability": true,
+        "beds": "1 King-Sized Beds",
+        "sleeps": 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
 
+      {
+        "hotel_id": 3,
+        "type": "Queen-Suite",
+        "list_price": "$400.00/nightly",
+        "current_price": "350.00/nightly",
+        "availability": false,
+        "beds": "1 Queen-Sized Bed",
+        "sleeps": 2,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
 
       {
-        "hotel_id": "5",
-        "type": "",
-        "list_price": "",
-        "current_price": "",
-        "availability": "",
-        "beds": "",
-        "sleeps": "",
+        "hotel_id": 1,
+        "type": "King-Suite",
+        "list_price": "300.00/nightly",
+        "current_price": "299.99/nightly",
+        "availability": true,
+        "beds": "2 King-Sized Beds",
+        "sleeps": 5,
         createdAt: new Date(),
         updatedAt: new Date()
       }],
@@ -71,12 +81,6 @@ module.exports = {
 
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+      return queryInterface.bulkDelete('Rooms', null, {});
   }
 };
