f262bf4942ea9f8ef4d2011b6f5fd1151d886f74
jngong <34215359+jngong@users.noreply.github.com>
2020-02-18 21:16:54 -0500

    moved all state and methods from App.js to Home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 
@@ -7,76 +7,18 @@ import HotelList from './components/routes/HotelList'
 import Hotel from './components/routes/Hotel'
 import Confirmation from './components/routes/Confirmation'
 
-class App extends Component {
+//TODO: Move all state to Home component and implement a redirect to pass state from Home to the next Route component (i.e. HotelList)
 
-  // Set up App as a class component to set state - initially with dummy data for search terms.
-  constructor(props) {
-    super(props)
-    this.state = {
-      tripSearch: {
-        departCity: 'Miami',
-        destCity: 'Los Angeles',
-        departDate: '06/02/2020',
-        returnDate: '06/09/2020',
-        travelers: 2,
-        hotelCheckbox: false,
-        ecoFriendlyCheckbox: true
-      },
-      cityId: 2 
-    }
-    this.handleTextChange = this.handleTextChange.bind(this)
-    this.handleCheckChange = this.handleCheckChange.bind(this)
-  }
-
-   //Set up handleChange function to capture data from form input fields
-      //For checkboxes, need to set state for tripSearch.[name] to checked.
-      //For all other values, need to set state for tripSearch.[name] to value.
-
-  handleTextChange = event => {
-    const {value, name} = event.target;
-    this.setState(prevState => ({
-      tripSearch: {
-        ...prevState.tripSearch,
-        [name]: value
-      }
-    }))
-    console.log(name, value);
-  }
-
-  handleCheckChange = (event) => {
-    const checked = event.target.checked
-    const name = event.target.name
-    this.setState(prevState => ({
-      tripSearch: {
-        ...prevState.tripSearch,
-        [name]: checked
-      }
-    }))
-  }
- 
-  handleSubmit = (event) => {
-    event.preventDefault()
-    //When the form is submitted, it needs to do two things. 
-    // 1: compare the destCity value to our list of cities to set the state of CityId and 
-    // 2: push to the next route in the user flow (HotelList)
-      this.props.history.push(`/hotels`)
-  }
-
-  render() {
-
-    return (
-      <React.Fragment>
-        {/* Corey changed the Home route to not pass down other props as that was causing the page to re-render, losing focus on the search input fields. */}
-        <Route exact path='/'>
-          <Home tripSearch={this.state.tripSearch} handleTextChange={this.handleTextChange} handleCheckChange={this.handleCheckChange} handleSubmit={this.handleSubmit} />
-        </Route>
-
-        <Route exact path='/hotels' component={HotelList} />
-        <Route exact path='/hotels/:hotel_id' component={Hotel} />
-        <Route exact path='/hotels/:hotel_id/confirmation' component={Confirmation} />
-      </React.Fragment>
-    );
-  }
+const App = () => {
+  return (
+    <React.Fragment>
+      {/* Corey changed the Home route to not pass down other props as that was causing the page to re-render, losing focus on the search input fields. */}
+      <Route exact path='/' component={Home} />
+      <Route exact path='/hotels' component={HotelList} />
+      <Route exact path='/hotels/:hotel_id' component={Hotel} />
+      <Route exact path='/hotels/:hotel_id/confirmation' component={Confirmation} />
+    </React.Fragment>
+  );
 
 }
 
--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -1,25 +1,104 @@
 import React from 'react'
+import { Redirect } from 'react-router-dom'
+import axios from 'axios'
+
 import SearchOptionsNav from '../home-subcomponents/SearchOptionsNav'
 import SearchVacationPackagesNav from '../home-subcomponents/SearchVacationPackagesNav'
 import SearchForm from '../home-subcomponents/SearchForm'
+
 import '../../Home.css'
 
-const Home = (props) => {
-    console.log(props)
-    return(
-        <div>
-            <h1>Placeholder for LogoNav component</h1>
-            <SearchOptionsNav />
-            <SearchVacationPackagesNav />
-            <SearchForm 
-                tripSearch={props.tripSearch} 
-                handleTextChange={props.handleTextChange}
-                handleCheckChange={props.handleCheckChange} 
-                handleSubmit={props.handleSubmit}
-            />
-            <p>Book together and SAVE!</p>
-        </div>
-    )
+// Change Log: Instead of App being our stateful component, changed Home to be a stateful component to allow it to return a Redirect upon Submit of the Search form.
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            tripSearch: {
+                departCity: 'Miami',
+                destCity: 'Los Angeles',
+                departDate: '06/02/2020',
+                returnDate: '06/09/2020',
+                travelers: 2,
+                hotelCheckbox: false,
+                ecoFriendlyCheckbox: true
+            },
+            cityId: 2,
+            cities: [],
+            redirect: false
+        }
+        this.handleTextChange = this.handleTextChange.bind(this)
+        this.handleCheckChange = this.handleCheckChange.bind(this)
+    }
+
+    async componentDidMount() {
+        try {
+            const response = await axios(`http://localhost:3001/cities`)
+            this.setState({
+                cities: response.data.cities
+            })
+            console.log(response.data.cities)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+
+    //Set up handleChange function to capture data from form input fields
+    //For checkboxes, need to set state for tripSearch.[name] to checked.
+    //For all other values, need to set state for tripSearch.[name] to value.
+
+    handleTextChange = event => {
+        const { value, name } = event.target;
+        this.setState(prevState => ({
+            tripSearch: {
+                ...prevState.tripSearch,
+                [name]: value
+            }
+        }))
+        console.log(name, value);
+    }
+
+    handleCheckChange = (event) => {
+        const checked = event.target.checked
+        const name = event.target.name
+        this.setState(prevState => ({
+            tripSearch: {
+                ...prevState.tripSearch,
+                [name]: checked
+            }
+        }))
+    }
+
+    //When the form is submitted, it needs to do two things. 
+    // 1: compare the destCity value to our list of cities to set the state of CityId and 
+    // 2: push to the next route in the user flow (HotelList)
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        // if (redirect){
+        //     return (<Redirect to={{pathname: '/hotels', tripSearch: this.state.tripSearch, cityId: this.state.cityId}} />)
+        // }
+    }
+
+    render() {
+        return (
+            <div className="home" >
+                <h1>Placeholder for LogoNav component</h1>
+                <SearchOptionsNav />
+                <SearchVacationPackagesNav />
+                <SearchForm
+                    tripSearch={this.state.tripSearch}
+                    handleTextChange={this.state.handleTextChange}
+                    handleCheckChange={this.state.handleCheckChange}
+                    handleSubmit={this.state.handleSubmit}
+                />
+                <p>Book together and SAVE!</p>
+            </div>
+        )
+    }
 
 }
 
