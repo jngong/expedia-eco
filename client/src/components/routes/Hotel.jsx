2a1d0ff3af7c0e0035b9dc699d2b37b309b40b53
Ro <hohenkirkrhol@gmail.com>
2020-02-19 11:43:55 -0500

    Hotel component mostly complete.

--- client/src/components/routes/Hotel.jsx
+++ client/src/components/routes/Hotel.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
-export const Hotel = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+
+
+export default class Hotel extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            Hotel: {}
+             
+        }
+    }
+
+    async componentDidMount() {
+            try {
+                const response = await axios.get("http://localhost:3001/hotels/2");
+                this.setState({Hotel: response.data.hotel})
+                console.log(response);
+            } catch (error) {
+                console.error(error);
+            }       
+    }
+    
+    render() {
+        return (
+            <div>
+
+                <h1>EXPEDIA LOGO AND BANNER RIGHT HERE (Pulled from LogoNav component)</h1>
+
+                <div id="hotel-component-banner">
+                    <p>See all hotels (Link)</p>
+                    <p id="expedia-phone-number">1-877-387-1531</p>
+                </div>
+
+                <div> (Icon Image Here) 2 of 4: Choose your room </div>
+
+                <div id="hotel-main-content">
+                    <p>{this.state.Hotel.name} Rating: {this.state.Hotel.rating}</p>
+                    <p>{this.state.Hotel.address}</p>
+                    <img src={require("../../images/hotel_room_image.jpg")} />
+                    {/* Need to add the scroll arrows icons */}
+                    <p>Very Good! {this.state.Hotel.rating}</p>
+                    <p>View all 1,769 Expedia Verified Reviews</p>
+                </div>
+
+                <div>
+                    <p>(Conditional rendering of the icons HERE.)</p>
+                </div>
+
+                <p>The hotel offers a coffee shop/cafe. A computer station is located on site
+                and wireless internet access is complimentary.</p>
+
+                <p>See more</p>
+
+            </div>
+        )
+    }
 }
+
+
