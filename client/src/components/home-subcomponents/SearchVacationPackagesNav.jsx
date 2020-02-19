45d189107a92aeb7a1d4389ad104a5c7cbbf04c4
jngong <34215359+jngong@users.noreply.github.com>
2020-02-19 17:48:52 -0500

    styled vacation packages nav

--- client/src/components/home-subcomponents/SearchVacationPackagesNav.jsx
+++ client/src/components/home-subcomponents/SearchVacationPackagesNav.jsx
@@ -3,11 +3,14 @@ import '../../Home.css'
 
 const SearchVacationPackagesNav = () => {
     return(
+        <div>
+            <h1 className="vacay-packages-header">Search Vacation Packages</h1>
         <div className="search-vacay-nav">
             <button className="search-vacay-nav-item">Flight + Hotel</button>
             <button className="search-vacay-nav-item">Flight + Hotel + Car</button>
             <button className="search-vacay-nav-item">Flight + Car</button>
             <button className="search-vacay-nav-item">Hotel + Car</button>
+        </div>
         </div>
     )
 }
