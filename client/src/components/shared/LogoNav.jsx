c40a94a08bd336caafeb0b99bd272852ba701ecd
jngong <34215359+jngong@users.noreply.github.com>
2020-02-20 14:27:15 -0500

    brought back Disney's styles on LogoNav and refactored component from Class to Functional

--- client/src/components/shared/LogoNav.jsx
+++ client/src/components/shared/LogoNav.jsx
@@ -1,29 +1,26 @@
 import React, { Component } from 'react'
 import Logo from '../../images/expedia-logo.png'
+import '../../css/LogoNav.css'
 
 
-class LogoNav extends Component {
+const LogoNav = () => {
 
-    
-    render() {
-        
-        return(
-            <div className='navbar'>
-                <div className='logo'>
-                    <img 
-                        src={Logo}
-                        alt='Expedia'
-                        width='140'
-                        height='40'  />
-                </div>        
-                <div className='nav-icons'>
-                    <i className="fas fa-heart"></i>
-                    <i className="fas fa-user"></i>
-                    <i className="fas fa-bars"></i>
-                </div>
+    return (
+        <div className='navbar'>
+            <div className='logo'>
+                <img
+                    src={Logo}
+                    alt='Expedia'
+                    width='140'
+                    height='40' />
             </div>
-        )
-    }
+            <div className='nav-icons'>
+                <i className="fas fa-heart"></i>
+                <i className="fas fa-user"></i>
+                <i className="fas fa-bars"></i>
+            </div>
+        </div>
+    )
 }
 
 export default LogoNav
