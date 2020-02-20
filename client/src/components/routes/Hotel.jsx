7e37f7a2c49b99c54c08720610446f88f7fcf32b
Jeffrey Lan <jtlan.sf@gmail.com>
2020-02-19 19:45:45 -0500

    merge error fix

--- client/src/components/routes/Hotel.jsx
+++ client/src/components/routes/Hotel.jsx
@@ -23,6 +23,7 @@ export default class Hotel extends Component {
   }
 
   render() {
+    console.log("props", this.props);
     return (
       <div>
         <div id="hotel-component-banner">
@@ -57,46 +58,6 @@ export default class Hotel extends Component {
           <TripDetails />
           <RoomButton />
         </div>
-      </div>
-    );
-  }
-
-  render() {
-    console.log("button props", this.props);
-    return (
-      <div>
-        <h1>
-          EXPEDIA LOGO AND BANNER RIGHT HERE (Pulled from LogoNav component)
-        </h1>
-
-        <div id="hotel-component-banner">
-          <p>See all hotels (Link)</p>
-          <p id="expedia-phone-number">1-877-387-1531</p>
-        </div>
-
-        <div> (Icon Image Here) 2 of 4: Choose your room </div>
-
-        <div id="hotel-main-content">
-          <p>
-            {this.state.Hotel.name} Rating: {this.state.Hotel.rating}
-          </p>
-          <p>{this.state.Hotel.address}</p>
-          <img src={require("../../images/hotel_room_image.jpg")} alt="hotel" />
-          {/* Need to add the scroll arrows icons */}
-          <p>Very Good! {this.state.Hotel.rating}</p>
-          <p>View all 1,769 Expedia Verified Reviews</p>
-        </div>
-
-        <div>
-          <p>(Conditional rendering of the icons HERE.)</p>
-        </div>
-
-        <p>
-          The hotel offers a coffee shop/cafe. A computer station is located on
-          site and wireless internet access is complimentary.
-        </p>
-
-        <p>See more</p>
       </div>
     );
   }
