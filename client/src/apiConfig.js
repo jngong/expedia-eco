fe4fb79862678803cfae2790191fef79f4535a31
jngong <34215359+jngong@users.noreply.github.com>
2020-02-20 16:18:31 -0500

    updated LogoNav css to match icon sizes on UX mockups

---
+++ client/src/apiConfig.js
@@ -0,0 +1,18 @@
+let apiUrl
+
+const apiUrls = {
+    production: 'https://expedia-flying-four.herokuapp.com',
+    deployment: 'http://localhost:3001'
+}
+
+if (window.location.hostname === 'localhost') {
+    apiUrl = apiUrls.deployment
+} else {
+    apiUrl = apiUrls.production
+}
+
+export default apiUrl
+
+/* 
+    This apiConfig file provides a variable for our API calls to the back-end. While we are working in deployment (i.e. accessing our back-end via localhost), the root of our URL will be localhost:3001. When in production, it will switch to our live heroku link.
+ */
