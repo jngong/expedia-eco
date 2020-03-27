45ec647a93728ff5b13a9fe3d5b257bf74fa7c6d
jngong <34215359+jngong@users.noreply.github.com>
2020-03-27 12:44:33 -0400

    ran client build again and updated server.js

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  // Handle React routing, return all requests to React app
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app - any wildcard requests will be re-routed to React
+  app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
